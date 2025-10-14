e739c629afb5390e3583cb653ed07f775370c632
Tashikv <vozhakovatasha1@gmail.com>
2025-10-15 01:27:08 +0500

    Программирование окна MainWindow. Вожакова

--- RegIN_Vozhakova/MainWindow.xaml.cs
+++ RegIN_Vozhakova/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RegIN_Vozhakova.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,20 +11,40 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace RegIN_Vozhakova
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public User UserLogIn = new User();
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            OpenPage(new Pages.Login());
+        }
+
+        public void OpenPage(Page page)
+        {
+            DoubleAnimation StartAnimation = new DoubleAnimation();
+            StartAnimation.From = 1;
+            StartAnimation.To = 0;
+            StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            StartAnimation.Completed += delegate
+            {
+                frame.Navigate(page);
+                DoubleAnimation EndAnimation = new DoubleAnimation();
+                EndAnimation.From = 0;
+                EndAnimation.To = 1;
+                EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityProperty, EndAnimation);
+            };
+            frame.BeginAnimation(Frame.OpacityProperty, StartAnimation);
         }
     }
 }
